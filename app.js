72e715f563ac68bded1357fc47aed4bcf6463daa
Dan Weissbard <danweissbard@gmail.com>
2017-08-03 19:20:52 -0400

    Clean upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan  = require('morgan');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const models = require('./models');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use('/', morgan('combined'));
+app.get('/', (req, res, next)=>res.render('index'));
+
+
+
+
+app.listen(port, ()=>{
+  console.log(`Listening on port ${port}`);
+});
